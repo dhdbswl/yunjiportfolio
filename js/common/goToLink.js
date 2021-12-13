6918f0208f1b3dcbcc4292cfeb8aba161b99e030
dhdbswl <yunz01@naver.com>
2021-12-14 04:52:48 +0900

    add portfolio page

--- js/common/goToLink.js
+++ js/common/goToLink.js
@@ -12,6 +12,14 @@ function linkNotion() {
 	window.open("https://spice-chemistry-b6a.notion.site/Web-Application-Developer-yunji-oh-5153132266124628a0a1fb483a76382b", "_blank")
 }
 
+// portfolio - caihong website
+function linkSiteCaihongWebsite() {
+	window.open("https://dhdbswl.github.io/portfolioCaihong/", "_blank")
+}
+function linkGithubCaihongWebsite() {
+	window.open("https://github.com/dhdbswl/portfolioCaihong", "_blank")
+}
+
 // portfolio - Online Shopping mall for Portfolio
 function linkSitePortfolioShoppingmall() {
 	window.open("http://teamlight.cafe24.com/", "_blank")
